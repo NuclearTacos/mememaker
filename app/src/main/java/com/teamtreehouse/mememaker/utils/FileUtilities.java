07e9a1d9fd14de1ab4e9c53bcc17fafe14e4e9e8
Chris Rea <chris.rea@cityfirst.church>
2018-06-29 20:00:24 -0500

    Added file to format JSON response from DarkSky.

--- app/src/main/java/com/teamtreehouse/mememaker/utils/FileUtilities.java
+++ app/src/main/java/com/teamtreehouse/mememaker/utils/FileUtilities.java
@@ -5,6 +5,7 @@ import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
+import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -21,8 +22,54 @@ import java.io.OutputStream;
 public class FileUtilities {
 
     public static void saveAssetImage(Context context, String assetName) {
+        File fileDirectory = getFileDirectory(context);
+        File fileToWrite = new File(fileDirectory, assetName);
 
+
+        AssetManager assetManager = context.getAssets();
+
+        try {
+            InputStream in = assetManager.open(assetName);
+            FileOutputStream out = new FileOutputStream(fileToWrite);
+
+            copyFile(in, out);
+
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static File getFileDirectory(Context context){
+        return context.getFilesDir();
+    }
+
+    public static File[] listFiles(Context context){
+        File fileDirectory = getFileDirectory(context);
+        File[] filteredFiles = fileDirectory.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                if (file.getAbsolutePath().contains(".jpg")){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+
+        return filteredFiles;
+    }
+
+    private static void copyFile(InputStream in, OutputStream out) throws IOException
+    {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+    }
+
 
     public static Uri saveImageForSharing(Context context, Bitmap bitmap,  String assetName) {
         File fileToWrite = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), assetName);
@@ -32,18 +79,19 @@ public class FileUtilities {
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
             outputStream.flush();
             outputStream.close();
+            return Uri.fromFile(fileToWrite);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+            return Uri.fromFile(fileToWrite);
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
             return Uri.fromFile(fileToWrite);
-        }
+        } //finally {return Uri.fromFile(fileToWrite);}
     }
 
 
     public static void saveImage(Context context, Bitmap bitmap, String name) {
-        File fileDirectory = context.getFilesDir();
+        File fileDirectory = getFileDirectory(context);
         File fileToWrite = new File(fileDirectory, name);
 
         try {
